cb3dc554b43e87834672a6700f094461c6afa6a9
Monika <monikasasi123@gmail.com>
2025-01-12 17:11:51 +0530

    updated job location and changed the pdf read to use loaded bytes isntead of file name

--- recommender.py
+++ recommender.py
@@ -8,8 +8,8 @@ nlp = spacy.load("en_core_web_sm")
 
 
 # Function to process the resume and recommend jobs
-def process_resume(file_path):
-    resume_text = extract_text_from_pdf(file_path)
+def process_resume(uploaded_file):
+    resume_text = extract_text_from_pdf(uploaded_file)
     skills = extract_skills(resume_text)
     recommendations = recommend_jobs(skills)
     return recommendations, skills
@@ -24,8 +24,8 @@ uploaded_file = st.file_uploader("Choose a file", type=['pdf'])
 
 if uploaded_file is not None:
     # Process resume and recommend jobs
-    file_path=uploaded_file.name
-    df_jobs, extracted_skills = process_resume(file_path)
+
+    df_jobs, extracted_skills = process_resume(uploaded_file)
 
     # Display recommended jobs as DataFrame
     st.write("Recommended Jobs:")
